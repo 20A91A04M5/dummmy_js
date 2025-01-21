ba61003a45c7f83238144abbd14c86c047130a42
20A91A04M5 <tirumalaprasad0333@gmail.com>
2025-01-22 00:21:23 +0530

    changed and addded 3rd task

--- contollers/usercontrol.js
+++ contollers/usercontrol.js
@@ -1,17 +1,49 @@
 
 var users=require("../models/user.js")
 
-exports.usersdata=async (req,res)=>{
+const getAll=async (req,res)=>{
     try{
-        var data=await users.usersdata()
-        if(data){
-            res.send("you are not accessed")
-        }
-        else{
-            res.send("no data")
-        }
+        var data=await users.findAll()
+        res.json(data)
     }
     catch(err){
         res.send(err)
     }
 };
+
+const createuser=async (req,res)=>{
+    try{
+        const newUser= await users.create(req.body);
+        res.json(newUser)
+    }
+    catch(error){
+        res.json(error)
+    }
+}
+
+const updateuser=async (req,res)=>{
+    try{
+        const updateuser=await users.update(req.params.id,req.body);
+        res.json(updateuser)
+    }
+    catch(error){
+        res.json(error)
+    }
+}
+
+const deleteuser=async (req,res)=>{
+    try{
+        const deleteuser=await users.delete(req.params.id);
+        res.json(deleteuser)
+    }
+    catch(error){
+        res.json(error)
+    }
+}
+
+module.exports= {
+    getAll,
+    createuser,
+    updateuser,
+    deleteuser
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,47 +3,58 @@
 const express = require('express');
 const User = require('../models/user');
 
+const usercontoller=require("../contollers/usercontrol")
+
 const router = express.Router();
 
-// Get all users
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
-});
+// Get all users  -- without using contollers
+// router.get('/', async (req, res) => {
+//   try {
+//     const users = await User.findAll();
+//     res.json(users);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Failed to fetch users' });
+//   }
+// });
+
+router.get('/',usercontoller.getAll);
 
 // Create a new user
-router.post('/', async (req, res) => {
-  try {
-    const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to create user' });
-  }
-});
+// router.post('/', async (req, res) => {
+//   try {
+//     const newUser = await User.create(req.body);
+//     res.status(201).json(newUser);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Failed to create user' });
+//   }
+// });
+
+router.post('/',usercontoller.createuser)
+
 
 // Update a user
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedUser = await User.update(req.params.id, req.body);
-    res.json(updatedUser);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to update user' });
-  }
-});
+// router.put('/:id', async (req, res) => {
+//   try {
+//     const updatedUser = await User.update(req.params.id, req.body);
+//     res.json(updatedUser);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Failed to update user' });
+//   }
+// });
+
+router.put('/:id',usercontoller.updateuser)
 
 // Delete a user
-router.delete('/:id', async (req, res) => {
-  try {
-    const message = await User.delete(req.params.id);
-    res.json(message);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to delete user' });
-  }
-});
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const message = await User.delete(req.params.id);
+//     res.json(message);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Failed to delete user' });
+//   }
+// });
+
+router.delete('/:id',usercontoller.deleteuser)
 
 module.exports = router;
 
